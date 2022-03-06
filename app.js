93b0e62b884a299c9a9fbcd9a755d58019278c16
user1 <67192434+song0708@users.noreply.github.com>
2022-03-06 09:58:35 +0800

    resume

---
+++ app.js
@@ -0,0 +1,45 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const navlogo = document.querySelector('#navbar__logo')
+
+//display Mobile Menu
+const mobileMenu = () =>{
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click',mobileMenu);
+
+//Show active menu when scrolling
+const highlightMenu = () =>{
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+
+    //adds 'highlight' class to my menu items
+    if(window.innerWidth > 960 && scrollPos < 600){ 
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }else if(window.innerWidth > 960 && scrollPos < 1400){
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
+    }
+    else if(window.innerWidth > 960 && scrollPos < 2345){
+        servicesMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600)||elem){
+        elem.classList.remove('highlight')
+
+    }
+}
+
+window.addEventListener('scroll',highlightMenu);
+window.addEventListener('click',highlightMenu);
