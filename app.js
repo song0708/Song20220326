8358aec62d4eea4a5adee9dc3fd0274c527698c5
user1 <67192434+song0708@users.noreply.github.com>
2022-03-08 16:04:33 +0800

    project-add

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const highlightMenu = () =>{
     const homeMenu = document.querySelector('#home-page')
     const aboutMenu = document.querySelector('#about-page')
     const servicesMenu = document.querySelector('#services-page')
+    const projectMenu = document.querySelector('#project-page')
     let scrollPos = window.scrollY
 
     //adds 'highlight' class to my menu items
@@ -32,9 +33,15 @@ const highlightMenu = () =>{
     else if(window.innerWidth > 960 && scrollPos < 2345){
         servicesMenu.classList.add('highlight')
         aboutMenu.classList.remove('highlight')
+        projectMenu.classList.remove('highlight')
         return
     }
-
+    else if(window.innerWidth > 960 && scrollPos < 3100){
+        projectMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
+    }
     if((elem && window.innerWidth < 960 && scrollPos < 600)||elem){
         elem.classList.remove('highlight')
 
